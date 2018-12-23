0a5cd73a426334ab04cd80cc4b5babe8469fc612
adelino <adc@pc58.home>
2018-12-23 17:50:14 +0100

    with functions

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	response.send('Application web pour le suivi des projets tutores')
+})
+
+const createNotification = notification => {
+	return admin
+		.firestore()
+		.collection('notifications')
+		.add(notification)
+		.then(doc => {
+			console.log('notification created', notification)
+		})
+}
+
+exports.postCreated = functions.firestore
+	.document('posts/{postId}')
+	.onCreate(doc => {
+		const post = doc.data()
+
+		const notification = {
+			content: 'Nouveau post',
+			user: `${post.first_name} ${post.last_name}`,
+			time: admin.firestore.FieldValue.serverTimestamp()
+		}
+		return createNotification(notification)
+		//
+	})
--- src/components/containers/CardConnexion.js
+++ src/components/containers/CardConnexion.js
@@ -4,14 +4,20 @@ const CardConnexion = () => {
 	return (
 		<div className="card z-depth-3">
 			<div className="card-content">
-				<div className="card-title grey-text text-darken-4 center">
-					Dashboard
+				<div className="card-title cyan-text text-darken-4 center">
+					Interface Web pour le Suivi des Projets Tutorés.
 				</div>
-				<p className="center grey-text text-darken-1">
-					Interface web pour le suivi des projets tutorés.
+
+				<p className="center grey-text text-darken-3">
+					Veuillez vous connecter avec votre compte Google, via le bouton
+					Connexion
 				</p>
-				<p className="center grey-text text-darken-1">
-					Veuillez vous connecter avec votre compte Google.
+				<p className="center grey-text text-darken-3">
+					Puis choisir votre sujet, via le bouton Sujets.
+				</p>
+				<p className="center grey-text text-darken-3">
+					A la fin de votre saisie pensez à vous deconnecter, via le bouton
+					Deconnexion.
 				</p>
 			</div>
 		</div>
