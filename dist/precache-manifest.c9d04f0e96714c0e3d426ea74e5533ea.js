b7607557f52abc8f7b951c5f7a6bcf70733f34ec
adelino <adc@MacBook-Pro-de-adelino.local>
2019-01-07 10:34:26 +0100

    notifications

---
+++ dist/precache-manifest.c9d04f0e96714c0e3d426ea74e5533ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "480085a36bff8f895ca7238ec6a59ef8",
+    "url": "/static/media/planet.480085a3.png"
+  },
+  {
+    "revision": "7daab030fcdff132997fe0440b134401",
+    "url": "/static/media/loading.7daab030.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9bf4b0d68bf85aa92a9",
+    "url": "/static/js/main.d9bf4b0d.chunk.js"
+  },
+  {
+    "revision": "8656834949d94f6792ff",
+    "url": "/static/js/1.86568349.chunk.js"
+  },
+  {
+    "revision": "d9bf4b0d68bf85aa92a9",
+    "url": "/static/css/main.d4e594ce.chunk.css"
+  },
+  {
+    "revision": "8656834949d94f6792ff",
+    "url": "/static/css/1.633ead4a.chunk.css"
+  },
+  {
+    "revision": "f03c8496214370dd3f4b2b5604f1d462",
+    "url": "/index.html"
+  }
+];
--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,8 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)
+const functions = require('firebase-functions');
 
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-// 	response.send('Application web pour le suivi des projets tutores')
-// })
-
-const createNotification = notification => {
-	return admin
-		.firestore()
-		.collection('notifications')
-		.add(notification)
-		.then(doc => {
-			console.log('notification created', notification)
-		})
-}
-
-exports.postCreated = functions.firestore
-	.document('posts/{postId}')
-	.onCreate(doc => {
-		const post = doc.data()
-
-		const notification = {
-			id: post.id,
-			content: post.sujet_name,
-			user: `${post.first_name} ${post.last_name}`,
-			time: admin.firestore.FieldValue.serverTimestamp()
-		}
-		return createNotification(notification)
-		//
-	})
+//  response.send("Hello from Firebase!");
+// });
