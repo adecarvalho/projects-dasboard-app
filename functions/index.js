77f6f122c090e890d4a8e350c8d4a277994e5057
adelino <adc@MacBook-Pro-de-adelino.local>
2019-01-07 11:03:07 +0100

    newFunctions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// 	response.send('Application web pour le suivi des projets tutores')
+// })
+
+const createNotification = notification => {
+	return admin
+		.firestore()
+		.collection('notifications')
+		.add(notification)
+		.then(doc => {
+			console.log('notification created', notification)
+		})
+}
+
+exports.postCreated = functions.firestore
+	.document('posts/{postId}')
+	.onCreate(doc => {
+		const post = doc.data()
+
+		const notification = {
+			id: post.id,
+			content: post.sujet_name,
+			user: `${post.first_name} ${post.last_name}`,
+			time: admin.firestore.FieldValue.serverTimestamp()
+		}
+		return createNotification(notification)
+		//
+	})
